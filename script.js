3f0347b694828ebff3641cc471814b0c5078cc83
minding-myown-business <github.com.pried334@passinbox.com>
2024-05-26 15:45:18 +0000

    add quick links

---
+++ script.js
@@ -0,0 +1,12 @@
+const gotoWordle = () => {
+  window.location.href = "https://www.nytimes.com/games/wordle/index.html";
+};
+const gotoGames = () => {
+  window.location.href = "https://www.coolmathgames.com/";
+};
+const gotoSchool = () => {
+  window.location.href = "https://login.arbor.sc/";
+};
+const cheatOnMyHomework = () => {
+  window.location.href = "https://chatgpt.com/";
+};
